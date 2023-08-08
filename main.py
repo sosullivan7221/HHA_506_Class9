697f77194f56d8b06615722c2ed56423dc210eb5
sosullivan7221 <sosullivan7221@gmail.com>
2023-08-08 19:24:51 -0400

    added new functions

---
+++ main.py
@@ -0,0 +1,10 @@
+import module1
+
+output1 = module1.addition(1, 2)
+output2 = module1.subtraction(1, 2)
+
+print(f'Output 1: {output1}')
+print(f"Output 2:: {output2}")
+
+
+
---
+++ module1.py
@@ -0,0 +1,8 @@
+def addition(var1, var2):
+    output = var1 + var2
+    return output
+
+def subtraction(var1, var2):
+    output = var1 - var2
+    return output
+
